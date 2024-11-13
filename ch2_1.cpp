bc3f501b4ea466f79fe345d13c79535a89b7f2e3
shuaizhu <1186366196@qq.com>
2024-11-13 22:58:11 +0800

    cpp

---
+++ ch2_1.cpp
@@ -0,0 +1,15 @@
+//
+// Created by shuaizhu on 2024/11/13.
+//
+#include <iostream>
+
+int main()
+
+{
+    int rate;
+    std::cout << "please rate from 1-10" << std::endl;
+
+    std::cin >> rate;
+    std::cout << "you rated this book as " << rate << std::endl;
+
+}
